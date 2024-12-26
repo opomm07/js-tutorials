61c9f8b3a3520456ea9ccdf9632d67d6bb7a106b
opomm07 <ommprakashdas1001@gmail.com>
2024-12-26 18:52:33 +0000

    variables concept learnt

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,12 @@
-console.log("opbhai")
+const accountId = 10743;
+let accountEmail = "ommprakashdas1001@gmail.com";
+var accountPassword = "chipichipichapachapa";
+accountCity = "jaipur";
+
+console.log(accountId);
+
+accountEmail = "ommprakashop@gmail.com";
+accountPassword = "dubidubiddabadaba";
+accountCity = "bhangarh";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
